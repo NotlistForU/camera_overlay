f5dd0419734085d6c764b86a7a608b91c84a9b79
GabrieOliveiraMartins <gabrolivmartins@gmail.com>
2026-01-23 13:36:37 -0400

    Salvando progresso
    Filtro de missão, não esta 100% ainda, a opção e todas missões não esta funcionando

--- lib/view/galeria/modal.dart
+++ lib/view/galeria/modal.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:sipam_foto/database/missoes/select.dart' as select;
 import 'package:sipam_foto/model/filtro.dart' as model;
+import 'package:sipam_foto/model/missao.dart' as model;
 
 class Filtros extends StatefulWidget {
   final model.Filtro filtro;
@@ -11,22 +13,43 @@ class Filtros extends StatefulWidget {
 
 class _FiltrosState extends State<Filtros> {
   late model.Filtro filtro;
-  final _input = TextEditingController();
+  late TextEditingController _nomeController;
   DateTime? _inicio;
   DateTime? _fim;
-  double? _min = 0;
-  double? _max = 2000;
+  double _min = 0;
+  double _max = 2000;
   RangeValues _altitudeRange = const RangeValues(0, 2000);
   int? _missaoid;
+  List<model.Missao> missoes = [];
+  bool loadingMissoes = true;
 
   @override
   void initState() {
     super.initState();
     filtro = widget.filtro;
+    _nomeController = TextEditingController(text: filtro.nome ?? '');
+    _loadMissoes();
+  }
+
+  @override
+  void dispose() {
+    _nomeController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _loadMissoes() async {
+    final lista = await select.Missao.todasMissoes();
+    setState(() {
+      missoes = lista;
+      loadingMissoes = false;
+    });
   }
 
   @override
   Widget build(BuildContext c) {
+    if (loadingMissoes) {
+      return const Center(child: CircularProgressIndicator());
+    }
     return Padding(
       padding: EdgeInsets.only(
         left: 16,
@@ -45,55 +68,102 @@ class _FiltrosState extends State<Filtros> {
 
           TextField(
             decoration: const InputDecoration(labelText: 'Nome'),
+            controller: _nomeController,
             onChanged: (v) {
-              filtro = filtro.copyWith(nome: v.isEmpty ? null : v);
+              final texto = v.trim().toLowerCase();
+              filtro = filtro.copyWith(nome: texto.isEmpty ? null : texto);
             },
           ),
           const SizedBox(height: 12),
-          OutlinedButton(
-            onPressed: () async {
-              _inicio = await showDatePicker(
-                context: c,
-                initialDate: filtro.inicio ?? DateTime.now(),
-                firstDate: DateTime(2000),
-                lastDate: DateTime.now(),
-              );
-              if (_inicio != null) {
+
+          DropdownButtonFormField<int>(
+            key: ValueKey(filtro.missaoid ?? -1),
+            initialValue: filtro.missaoid ?? -1,
+            isExpanded: true,
+            decoration: InputDecoration(
+              labelText: 'Missão',
+              filled: true,
+              fillColor: const Color.fromARGB(255, 40, 50, 70),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+            ),
+            items: [
+              const DropdownMenuItem<int>(
+                value: -1,
+                child: Text('Todas as missões'),
+              ),
+              ...missoes.map(
+                (m) => DropdownMenuItem<int>(value: m.id, child: Text(m.nome)),
+              ),
+            ],
+            onChanged: (v) {
+              if (v == -1) {
                 setState(() {
-                  filtro = filtro.copyWith(inicio: _inicio);
+                  filtro = filtro.copyWith(missaoid: null);
+                });
+              } else {
+                setState(() {
+                  filtro = filtro.copyWith(missaoid: v);
                 });
               }
             },
-            child: Text(
-              filtro.inicio == null
-                  ? 'Data inicial'
-                  : 'Início: ${filtro.inicio!.day}/${filtro.inicio!.month}/${filtro.inicio!.year}',
-            ),
           ),
-          OutlinedButton(
-            onPressed: () async {
-              _fim = await showDatePicker(
-                context: c,
-                initialDate: filtro.fim ?? DateTime.now(),
-                firstDate: filtro.inicio ?? DateTime(2000),
-                lastDate: DateTime.now(),
-              );
-              if (_fim != null) {
-                setState(() {
-                  filtro = filtro.copyWith(fim: _fim);
-                });
-              }
-            },
-            child: Text(
-              filtro.fim == null
-                  ? 'Data final'
-                  : 'Fim: ${filtro.fim!.day}/${filtro.fim!.month}/${filtro.fim!.year}',
-            ),
+
+          Row(
+            children: [
+              Expanded(
+                child: OutlinedButton(
+                  onPressed: () async {
+                    _inicio = await showDatePicker(
+                      context: c,
+                      initialDate: filtro.inicio ?? DateTime.now(),
+                      firstDate: DateTime(2000),
+                      lastDate: DateTime.now(),
+                    );
+                    if (_inicio != null) {
+                      setState(() {
+                        filtro = filtro.copyWith(inicio: _inicio);
+                      });
+                    }
+                  },
+                  child: Text(
+                    filtro.inicio == null
+                        ? 'Data inicial'
+                        : 'Início: ${filtro.inicio!.day}/${filtro.inicio!.month}/${filtro.inicio!.year}',
+                  ),
+                ),
+              ),
+              const SizedBox(width: 8), // espaço entre os botões
+              Expanded(
+                child: OutlinedButton(
+                  onPressed: () async {
+                    _fim = await showDatePicker(
+                      context: c,
+                      initialDate: filtro.fim ?? DateTime.now(),
+                      firstDate: filtro.inicio ?? DateTime(2000),
+                      lastDate: DateTime.now(),
+                    );
+                    if (_fim != null) {
+                      setState(() {
+                        filtro = filtro.copyWith(fim: _fim);
+                      });
+                    }
+                  },
+                  child: Text(
+                    filtro.fim == null
+                        ? 'Data final'
+                        : 'Fim: ${filtro.fim!.day}/${filtro.fim!.month}/${filtro.fim!.year}',
+                  ),
+                ),
+              ),
+            ],
           ),
+
           RangeSlider(
             values: _altitudeRange,
-            min: 0,
-            max: 2000,
+            min: _min,
+            max: _max,
             divisions: 1000,
             labels: RangeLabels(
               '${_altitudeRange.start.round()}m',
